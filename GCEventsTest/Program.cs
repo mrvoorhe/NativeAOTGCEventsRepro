6c3f304c0f6685727911fff1fd9af19ff6582c8a
Michael Voorhees <michaelv@unity3d.com>
2023-02-17 16:21:58 -0500

    Add reproduction project

---
+++ GCEventsTest/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics.Tracing;
+
+using var listener = new GCEventListener();
+
+Console.WriteLine("Hello, World!");
+
+System.Text.StringBuilder sb = new();
+for (int i = 0; i < 1000000; i++)
+{
+    sb.Append(i.ToString());
+}
+
+Console.WriteLine("Done");
+
+internal class GCEventListener : EventListener
+{
+    protected override ClrRuntimeEventKeywords ListenForKeywords => ClrRuntimeEventKeywords.GC;
+    
+    protected override void OnEventWritten(EventWrittenEventArgs eventData)
+    {
+        if (eventData.EventName != null && eventData.EventName.StartsWith("GCStart"))
+        {
+            var generation = (uint)eventData.Payload![1]!;
+            Console.WriteLine($"GC - Gen {generation}");
+        }
+        else if (eventData.EventName != null && eventData.EventName.StartsWith("GCEnd"))
+        {
+            var generation = (uint)eventData.Payload![1]!;
+            Console.WriteLine($"GC End - Gen - {generation}");
+        }
+    }
+}
+
+public abstract class EventListener : System.Diagnostics.Tracing.EventListener
+{
+    private EventSource? _subscribedEventSource;
+
+    [Flags]
+    protected enum ClrRuntimeEventKeywords
+    {
+        GC = 0x1,
+    }
+
+    // This must return a const or static value
+    // OnEventSourceCreated is called from the EventLister::.ctor, so this
+    // property will be called before the derived class constructor has run
+    protected abstract ClrRuntimeEventKeywords ListenForKeywords { get; }
+
+    protected override void OnEventSourceCreated(EventSource eventSource)
+    {
+        Console.WriteLine($"Got Event Source");
+        switch (eventSource.Name)
+        {
+            case "Microsoft-Windows-DotNETRuntime":
+                EnableEvents(eventSource, EventLevel.Informational, (EventKeywords)ListenForKeywords);
+                _subscribedEventSource = eventSource;
+                break;
+        }
+    }
+
+    public override void Dispose()
+    {
+        if (_subscribedEventSource != null)
+            DisableEvents(_subscribedEventSource);
+
+        base.Dispose();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# NativeAOTGCEventsRepro
-Repro project for a bug with nativeaot and gc events
+To Reproduce
+
+1) `dotnet publish`
+
+2) `GCEventsTest\bin\Debug\net7.0\win-x64\publish\GCEventsTest.exe`
+
+3) Output will be
+```
+Hello, World!
+Done
+```
+
+This is not expected.
+
+The expected output is
+```
+Got Event Source
+Got Event Source
+Hello, World!
+Got Event Source
+GC - Gen 0
+GC End - Gen - 0
+GC - Gen 1
+GC End - Gen - 1
+GC - Gen 0
+GC End - Gen - 0
+GC - Gen 0
+GC End - Gen - 0
+GC - Gen 0
+GC End - Gen - 0
+GC - Gen 2
+GC End - Gen - 2
+Done
+Got Event Source
+```
+
+You can see the expected output by running a build that does not use NativeAOT.
+
+```
+dotnet run --project GCEventsTest\GCEventsTest.csproj
+```
+
+or
+```
+dotnet publish -p:ShowExpectedBehavior=true --runtime win-x64 --self-contained
+GCEventsTest\bin\Debug\net7.0\win-x64\publish\GCEventsTest.exe
+```
+
+
